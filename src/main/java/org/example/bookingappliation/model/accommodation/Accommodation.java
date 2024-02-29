563b5c68c437b63128c66b57d7095d41d40ce845
IhorTrokhymchuk <hameleon009@gmail.com>
2024-02-29 18:41:15 +0200

    Added model Accommodation

---
+++ src/main/java/org/example/bookingappliation/model/accommodation/Accommodation.java
@@ -0,0 +1,47 @@
+package org.example.bookingappliation.model.accommodation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Set;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = TRUE WHERE id = ?")
+@Where(clause = "is_deleted = FALSE")
+@Table(name = "accommodations")
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_id", nullable = false)
+    private AccommodationType type;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "accommodations_amenity_types",
+            joinColumns = @JoinColumn(name = "accommodation_id"),
+            inverseJoinColumns = @JoinColumn(name = "amenity_type_id")
+    )
+    private Set<AmenityType> amenities;
+    @Column(name = "daily_rate", nullable = false)
+    private BigDecimal dailyRate;
+    @Column(name = "is_deleted",nullable = false)
+    private boolean isDeleted = false;
+}
---
+++ src/main/java/org/example/bookingappliation/model/accommodation/SizeType.java
@@ -0,0 +1,20 @@
+package org.example.bookingappliation.model.accommodation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "size_types")
+public class SizeType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
