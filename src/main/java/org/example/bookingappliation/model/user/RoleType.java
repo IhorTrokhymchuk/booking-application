b24782acff25ec17ff5ed8e3a107997f07be29e9
IhorTrokhymchuk <hameleon009@gmail.com>
2024-02-29 18:41:57 +0200

    Added model User

---
+++ src/main/java/org/example/bookingappliation/model/user/RoleType.java
@@ -0,0 +1,37 @@
+package org.example.bookingappliation.model.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+@Table(name = "role_types")
+public class RoleType implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",
+            nullable = false,
+            unique = true,
+            columnDefinition = "varchar")
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+
+    @Override
+    public String getAuthority() {
+        return name.name();
+    }
+
+    public enum RoleName {
+        CUSTOMER,
+        ADMIN
+    }
+}
