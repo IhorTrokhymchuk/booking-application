76cc2e400513067c338d6f27fc3effdd4fb67c78
IhorTrokhymchuk <hameleon009@gmail.com>
2024-02-29 18:15:27 +0200

    Initial commit

---
+++ src/main/java/org/example/bookingappliation/BookingAppliationApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookingappliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppliationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppliationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookingappliation/BookingAppliationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookingappliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppliationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
